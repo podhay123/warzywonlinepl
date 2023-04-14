8a259c2638dd26d10a42ded2112d037db0cb51da
podhay123 <kuba.pod123@gmail.com>
2023-04-14 21:36:04 +0200

    views etc

--- warzywoonlinepl/urls.py
+++ warzywoonlinepl/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("seller_page/", include("main_seller.urls")),
+    path("", include("main.urls")),
 ]
