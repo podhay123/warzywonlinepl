9fffd8ed728a868624a06b867db2f33a1eaf0eff
raksOtoOskar <oskarfryc@gmail.com>
2023-04-18 00:05:58 +0200

    changes to html files

--- users/views.py
+++ users/views.py
@@ -4,4 +4,17 @@ from django.contrib import messages
 
 
 def login_user(request):
-    return render(request, "users/authentication/login.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+
+            # return render(request, "main\\templates\\main\\index.html")
+        else:
+            messages.success(request, ("There Was An Error Loggin In, Try Again"))
+            return redirect("login")
+    else:
+        return render(request, "users/authentication/login.html", {})
