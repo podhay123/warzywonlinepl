1d528ff772ae05a6a6470c2a6c904c1f2689344b
podhay123 <69538690+podhay123@users.noreply.github.com>
2023-04-17 19:45:24 +0200

    Update views.py

--- users/views.py
+++ users/views.py
@@ -3,6 +3,5 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
-# Create your views here.
 def login_user(request):
     return redirect(request, "authentication/login.html")
