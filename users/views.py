250c2f33331194000cb79f6653d0c1d0025c0a32
podhay123 <kuba.pod123@gmail.com>
2023-04-17 20:21:55 +0200

    initial migration

--- users/views.py
+++ users/views.py
@@ -4,4 +4,14 @@ from django.contrib import messages
 
 
 def login_user(request):
-    return render(request, "users/authentication/login.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            messages.success(request, ("There Was An Error Loggin In, Try Again"))
+    else:
+        return render(request, "users/authentication/login.html")
