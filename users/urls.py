7b3c20b2c8fce24053dcc2096990bb0080b99d27
podhay123 <kuba.pod123@gmail.com>
2023-04-26 21:50:18 +0200

    added registration system and stuff

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from users import views
 app_name = "users"
 urlpatterns = [
     path("login_user/", views.login_user, name="login"),
-    path("", views.logout_user, name="logout"),
+    path("", views.logout_user, name="logout"),  # login_user/????
+    path("register_user/", views.register_user, name="register"),
 ]
