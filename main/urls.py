b29213729de5f77683e7c4cf02efddd3280385a3
raksOtoOskar <oskarfryc@gmail.com>
2023-04-28 23:48:19 +0200

    started making product page

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path("", views.main, name="main"),
-    path("", views.main, name="main"),
+    path("product_page/", views.product_page, name="product_page"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from main.models import Product
 
 
 # Create your views here.
 def main(request):
     return render(request, "main/index.html")
+
+
+def product_page(request):
+    product = Product.objects.all()
+    return render(request, "main/product_page.html", {"product": product})
