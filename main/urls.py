efda06cf7de54a68e9ece9d5055e44edd513a31f
raksOtoOskar <oskarfryc@gmail.com>
2023-05-16 22:14:09 +0200

    small changes in views

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.main, name="main"),
     path("sell/", views.sell, name="sell"),
     path("products/", views.all_products, name="all_products"),
+    path("products/<str:category>", views.products_category, name="products_category"),
     path("products/<str:name>/", views.product_page, name="product_page"),
     path("search_product/", views.search_product, name="search_product"),
     path("profil", views.account, name="account"),
